46159c26b86fdcecf44657b5785f5861ed59634e
guilhermearruda98 <86967771+guilhermearruda98@users.noreply.github.com>
2023-04-14 23:34:35 -0300

    Corrigido commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,13 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		for (int i = 0; i < 5; i++) {
-<<<<<<< .mine
-			System.out.println("Error");
-||||||| .r4
-			System.out.println("Terceiro commit");
-=======
-			System.out.println("Commit paralelo");
->>>>>>> .r5
+			System.out.println("Commit correto");
 		}
 	}
 }
